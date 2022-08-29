ae64f633f3a580a7dc57d536e7ec3f5196eddd4f
DC-22 <105130027+DC-22@users.noreply.github.com>
2022-08-29 12:37:05 +0530

    Social Media Clone

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData= [
+  {
+    name: "Minions",
+    shares: 97,
+  },
+  {
+    name: "Avangers",
+    shares: 80.5,
+  },
+  {
+    name: "Anime",
+    shares: 75.5,
+  },
+  {
+    name: "OPM",
+    shares: 72,
+  },
+  {
+    name: "Elon Musk",
+    shares: 71.9,
+  },
+  {
+    name: "JJK-0",
+    shares: 20,
+  },
+];
